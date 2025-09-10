b2cf7d40f2b0dd1f9cad7f28b3e86de8a8366169
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2025-09-09 21:17:46 -0300

    Alteração banco.sql

--- primeiroprojeto/banco.sql
+++ primeiroprojeto/banco.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS `laravel`.`clientes` (
   `updated_at` TIMESTAMP NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+CREATE TABLE categorias (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NULL DEFAULT NULL,
+    updated_at TIMESTAMP NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE clientes
+ADD COLUMN categoria_id BIGINT UNSIGNED AFTER id,
+ADD CONSTRAINT clientes_categoria_id_foreign
+    FOREIGN KEY (categoria_id)
+    REFERENCES categorias(id)
+    ON DELETE CASCADE;
